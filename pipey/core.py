cc1a089c75e989cab6b0a358c6ad03ca0614c28d
Adam Beckmeyer <abeckmeyer@auburn.edu>
2016-05-03 22:06:47 -0400

    refactoring code with helper var for readability

--- pipey/core.py
+++ pipey/core.py
@@ -4,26 +4,27 @@ class Network:
     '''The entire network of pipes'''
 
     def __init__(self):
-        self.segments = dict()
-        self.nodes = dict()
+        self.segments = dict() #this will hold all PipeSegments in the Network
+        self.nodes = dict() #this will hold all Nodes in the Network
 
     def load(self, filename):
         '''Creates a list of PipeSegments and Nodes from information in filename'''
 
         with f as open(filename, 'r'):
             for line in f:
-                if line: #Check to make sure not empty line
+                list_line = line.split() #much easier to work list for this
+                if list_line: #Check to make sure not empty line
                     #For every line with the 'segment' or 'node' label create a new
                     #object and set as the object currently being modified
-                    if line.split()[0] == 'segment':
-                        self.segments[line.split()[-1]] = PipeSegment(line.split()[-1])
-                        current = self.segments[line.split()[-1]]
-                    elif line.split()[0] == 'node':
-                        self.nodes[line.split()[-1]] = Node(line.split()[-1])
-                        current = self.nodes[line.split()[-1]]
+                    if list_line[0] == 'segment':
+                        self.segments[list_line[-1]] = PipeSegment(list_line[-1])
+                        current = self.segments[list_line[-1]]
+                    elif list_line[0] == 'node':
+                        self.nodes[list_line[-1]] = Node(list_line[-1])
+                        current = self.nodes[list_line[-1]]
                     else:
                         #add element to segment or node
-                        current.add(line.split())
+                        current.add(list_line)
 
 
 class PipeSegment:
