7a5e4029731c1644fb0eae97268b07b84a810102
Adam Beckmeyer <adam_git@thebeckmeyers.xyz>
2016-11-01 18:46:08 -0400

    Change license to GPLv3 and properly apply

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 '''A setuptools-based setup module for pipey'''
 
+# Copyright 2016 Adam Beckmeyer
+# 
+# This file is part of Pipey.
+# 
+# Pipey is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+# 
+# Pipey is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with Pipey.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
